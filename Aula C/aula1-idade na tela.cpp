9d6f1055e2efb3c76b5cdf19befbed413c71d191
Gabriellyvict <gabriellyvictoriamoraesdeolive@gmail.com>
2026-08-28 16:40:49 -0300

    atividade em C
    atividades basicas em C

---
+++ Aula C/aula1-idade na tela.cpp
@@ -0,0 +1,17 @@
+	//inserindo biblioteca
+#include <stdio.h>
+
+	//iniciando o código C
+int main() {
+	int idade;
+	
+	printf("digite sua idade: ");
+	scanf("%d", &idade);
+	
+	printf("Minha idade é: %d", idade);
+	
+	
+	
+		//retornando 0 erros caso esteja rudo ok
+	return 0;
+}
